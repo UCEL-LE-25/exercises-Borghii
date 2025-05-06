beabcaddb0c589732859bec20dde053411b9544d
Borghi <137845283+Borghii@users.noreply.github.com>
2025-05-06 18:09:44 -0300

    add exercise struct

---
+++ Struct/main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_ESTUDIANTES 100
+#define MAX_NOMBRE 50
+
+struct Estudiante {
+    char nombre[MAX_NOMBRE];
+    int edad;
+    float notaFinal;
+};
+
+
+int agregarEstudiante(struct Estudiante estudiantes[], int *cantidad);
+
+
+int main() {
+    struct Estudiante estudiantes[MAX_ESTUDIANTES];
+    int cantidad = 0;
+    int opcion;
+
+    do {
+        printf("Seleccione opcion:\n");
+        printf("1. Agregar nuevo estudiante\n");
+        printf("2. Salir\n");
+        scanf("%d", &opcion);
+
+        switch (opcion) {
+            case 1:
+                agregarEstudiante(estudiantes, &cantidad);
+                break;
+            case 2:
+            printf("Saliendo del programa.\n");
+            break;
+
+            default:
+                printf("Opcion invalida.\n");
+        }
+
+    } while (opcion != 2);
+
+    return 0;
+}
+
+int agregarEstudiante(struct Estudiante estudiantes[], int *cantidad) {
+    if (*cantidad >= MAX_ESTUDIANTES) {
+        printf("No se pueden agregar mas estudiantes.\n");
+        return 0;
+    }
+
+    printf("Nombre: ");
+    scanf("%s", estudiantes[*cantidad].nombre);
+
+    printf("Edad: ");
+    scanf("%d", &estudiantes[*cantidad].edad);
+
+    printf("Nota final: ");
+    scanf("%f", &estudiantes[*cantidad].notaFinal);
+
+    (*cantidad)++;
+    return 1;
+}
